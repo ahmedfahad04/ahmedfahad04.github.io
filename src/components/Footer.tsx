89e6b63c50fd25d4eec992ed3e93c8afdf45efe6
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-12-07 01:39:43 +0600

    new portfolio files added

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-4">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">
+          ©️ All rights reserved by{" "}
+          <a
+            href="https://www.linkedin.com/in/ahmedfahad04/"
+            className="text-blue-600"
+          >
+            Istiaq Ahmed Fahad
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
