8010dd6c4e136312ff1d978a5bdd840ad21615cd
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2023-12-07 01:49:09 +0600

    project card color updated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -28,28 +28,6 @@ export default function Projects() {
         </div>
         <div className="flex flex-wrap -m-4 items-center justify-center">
           {projects.map((project) => (
-            // <a
-            //   href={project.link}
-            //   key={project.image}
-            //   className="sm:w-1/2 w-100 p-4"
-            // >
-            //   <div className="flex relative">
-            //     <img
-            //       alt="gallery"
-            //       className="absolute inset-0 w-full h-full object-cover object-center"
-            //       src={project.image}
-            //     />
-            //     <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100 transition-all ease-linear duration-200">
-            //       <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-            //         {project.subtitle}
-            //       </h2>
-            //       <h1 className="title-font text-lg font-medium text-white mb-3">
-            //         {project.title}
-            //       </h1>
-            //       <p className="leading-relaxed">{project.description}</p>
-            //     </div>
-            //   </div>
-            // </a>
             <a href={project.link} className="cursor-pointer">
               <div className="max-w-sm overflow-hidden shadow-lg m-3 bg-[#1F2937] rounded-lg ">
                 <img
@@ -67,20 +45,11 @@ export default function Projects() {
                   {project.subtitle.split(", ").map((lang, id) => (
                     <span
                       key={id}
-                      className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                      className="inline-block bg-secondary rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
                     >
                       {lang}
                     </span>
                   ))}
-                  {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #photography
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #travel
-                </span>
-                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  #winter
-                </span> */}
                 </div>
               </div>
             </a>
