0621b7aac6c1a49e0ea4fd678cb031b612274157
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-01-02 10:09:19 +0600

    add course content

---
+++ src/components/Course_Page/course_data.ts
@@ -0,0 +1,62 @@
+export const contents = [
+    {
+        module: 'Week 1: Getting Started with Python',
+        sub_module: [
+            'Introduction to Python and its applications',
+            'Installation and setup (Anaconda, Jupyter Notebooks)',
+            'Basic syntax, data types, and variables'
+        ]
+    },
+    {
+        module: 'Week 2: Control Flow and Functions',
+        sub_module: [
+            'Conditional statements (if, elif, else)',
+            'Loops (for, while)',
+            'Functions and their usage'
+        ]
+    },
+    {
+        module: 'Week 3: Python Data Structures',
+        sub_module: [
+            'Lists and their manipulation',
+            'Introduction to dictionaries'
+        ]
+    },
+    {
+        module: 'Week 4: Strings and Tuples',
+        sub_module: [
+            'Working with strings and string manipulation',
+            'Introduction to tuples'
+        ]
+    },
+    {
+        module: 'Week 5: Data Analysis with Pandas',
+        sub_module: [
+            'Introduction to Pandas',
+            'Pandas Series and DataFrame',
+            'Data cleaning and basic manipulation'
+        ]
+    },
+    {
+        module: 'Week 6: Data Exploration with Pandas',
+        sub_module: [
+            'Exploratory Data Analysis (EDA) with Pandas',
+            'Basic statistical analysis'
+        ]
+    },
+    {
+        module: 'Week 7: Data Visualization with Matplotlib',
+        sub_module: [
+            'Introduction to Matplotlib',
+            'Creating basic plots with Matplotlib',
+            'Customizing plots and adding labels'
+        ]
+    },
+    {
+        module: 'Week 8: Intermediate Data Visualization',
+        sub_module: [
+            'Visualization with Seaborn for enhanced aesthetics',
+            'Overview of other visualization libraries (optional)'
+        ]
+    }
+];
