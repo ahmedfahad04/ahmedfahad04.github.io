f84b2be771262002a227f34d40a401884590d5e6
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2024-01-02 08:34:43 +0600

    updated the falty image url

--- src/components/Course_Page/CourseCard.tsx
+++ src/components/Course_Page/CourseCard.tsx
@@ -3,7 +3,7 @@ export default function CourseCard() {
     <div className="relative flex flex-col mt-4 text-gray-700 bg-white shadow-lg shadow-slate-500 bg-clip-border rounded-xl w-[420px]">
       <div className="relative h-50 mx-4 mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40">
         <img
-          src="../../../public/course_cover_2.jpg"
+          src="../../../course_cover_2.jpg"
           width={400}
           height={400}
           alt="card-image"
@@ -11,7 +11,7 @@ export default function CourseCard() {
       </div>
       <div className="p-6">
         <h5 className="block mb-2 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-blue-gray-900">
-          Hands-on Python Basics & Data Science for Beginners
+          Hands-on Python and Basic Data Analysis for Absolute Beginners
         </h5>
         <p className="block font-sans text-base antialiased font-light leading-relaxed text-inherit">
           Unlock the world of programming with our hands-on Python Basics & Data
@@ -46,7 +46,7 @@ export default function CourseCard() {
             className=" w-[150px] flex flex-row justify-center items-center align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-white-500 text-green-500 border-green-500 border-2 shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
             type="button"
           >
-            <img src="../../../public/whatsapp_icon.png" width={25} />{" "}
+            <img src="../../../whatsapp_icon.png" width={25} />{" "}
             <span className="ml-2">WhatsApp</span>
           </button>
         </a>
@@ -56,7 +56,7 @@ export default function CourseCard() {
             className=" w-[150px] flex flex-row justify-center items-center align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-white-500 text-red-500 border-red-500 border-2 shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
             type="button"
           >
-            <img src="../../../public/gmail_icon.png" width={25} />{" "}
+            <img src="../../../gmail_icon.png" width={25} />{" "}
             <span className="ml-2">Email</span>
           </button>
         </a>
